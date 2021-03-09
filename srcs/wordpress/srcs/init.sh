6027ac2b42d885595d8cae59602bb977060b2d76
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-03-09 17:09:59 +0900

    feat(wordpress): add container image and manifest

---
+++ srcs/wordpress/srcs/init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+sed -e s/database_name_here/$WORDPRESS_DB_NAME/g \
+	-e s/username_here/$WORDPRESS_DB_USER/g \
+	-e s/password_here/$WORDPRESS_DB_PASSWORD/g \
+	-e s/localhost/$WORDPRESS_DB_HOST/g \
+	/var/www/html/wp-config-sample.php \
+	> /var/www/html/wp-config.php
+
+sed -i -e s/'user = nobody'/'user = nginx'/g \
+	-e s/'group = nobody'/'group = nginx'/g \
+	-e s/';listen.owner = nobody'/'listen.owner = nginx'/g \
+	-e s/';listen.group = nobody'/'listen.group = nginx'/g \
+	/etc/php7/php-fpm.d/www.conf
+
+sed -i -e s/';listen.group = nginx'/'listen.group = nginx'/g \
+	/etc/php7/php-fpm.d/www.conf
+
+php-fpm7
+nginx
+
+#tail -f /var/log/nginx/access.log
+tail -f /dev/null
