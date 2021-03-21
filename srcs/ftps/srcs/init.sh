f80666c8b067ad5ecaaf4949b4e6002c20ab73a4
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-03-21 15:54:57 +0900

    fix: telegraf conf

--- srcs/ftps/srcs/init.sh
+++ srcs/ftps/srcs/init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 adduser -h /var/lib/ftp -D $FTP_USER && echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-#echo -e "$FTP_PASSWORD\n$FTP_PASSWORD" | adduser -D $FTP_USER
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
